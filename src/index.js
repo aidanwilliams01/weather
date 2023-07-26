dd521adf8dea1a739c775eff8f7f54f47ef7b1f4
Aidan Williams <aidanwilliams01@gmail.com>
2023-07-26 16:18:11 -0700

    add US ZIP code search capability

--- src/index.js
+++ src/index.js
@@ -4,31 +4,38 @@ import './css/styles.css';
 
 // Business Logic
 
-function getWeather(city) {
+function getWeather(location) {
   let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`;
 
   request.addEventListener("loadend", function() {
     const response = JSON.parse(this.responseText);
     if (this.status === 200) {
-      printElements(response, city);
+      printElements(response, location);
     } else {
-      printError(this, response, city);
+      printError(this, response, location);
     }
   });
 
-  request.open("GET", url, true);
-  request.send();
+  if (location.search(/\d/) === 0) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${location}&appid=${process.env.API_KEY}&units=imperial`;
+    request.open("GET", url, true);
+    request.send();
+  }
+  else {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.API_KEY}&units=imperial`;
+    request.open("GET", url, true);
+    request.send();
+  }
 }
 
 // UI Logic
 
-function printError(request, apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+function printError(request, apiResponse, location) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${location}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
 }
 
-function printElements(apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `The weather in ${city} is '${apiResponse.weather[0].description}'.
+function printElements(apiResponse, location) {
+  document.querySelector('#showResponse').innerText = `The weather in ${location} is '${apiResponse.weather[0].description}'.
   The humidity is ${apiResponse.main.humidity}%.
   The temperature in Fahrenheit is ${apiResponse.main.temp} degrees.
   The feels-like temperature in Fahrenheit is ${apiResponse.main.feels_like} degrees.
@@ -37,9 +44,9 @@ function printElements(apiResponse, city) {
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const city = document.querySelector('#location').value;
+  const location = document.querySelector('#location').value;
   document.querySelector('#location').value = null;
-  getWeather(city);
+  getWeather(location);
 }
 
 window.addEventListener("load", function() {
