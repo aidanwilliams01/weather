2f0fa91fa0c9e89f96d473446b138ca5c66d41f7
Aidan Williams <aidanwilliams01@gmail.com>
2023-07-26 14:17:53 -0700

    add more weather data

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ import './css/styles.css';
 
 function getWeather(city) {
   let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`;
 
   request.addEventListener("loadend", function() {
     const response = JSON.parse(this.responseText);
     if (this.status === 200) {
-      const fahrenheitTemperature = (response.main.temp - 273.15) * 9/5 + 32;
-      printElements(response, city, fahrenheitTemperature);
+      printElements(response, city);
     } else {
       printError(this, response, city);
     }
@@ -28,10 +27,12 @@ function printError(request, apiResponse, city) {
   document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
 }
 
-function printElements(apiResponse, city, fahrenheitTemperature) {
-  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
-  The temperature in Kelvins is ${apiResponse.main.temp} degrees.;
-  The temperature in Fahrenheit is ${fahrenheitTemperature} degrees.`;
+function printElements(apiResponse, city) {
+  document.querySelector('#showResponse').innerText = `The weather in ${city} is '${apiResponse.weather[0].description}'.
+  The humidity is ${apiResponse.main.humidity}%.
+  The temperature in Fahrenheit is ${apiResponse.main.temp} degrees.
+  The feels-like temperature in Fahrenheit is ${apiResponse.main.feels_like} degrees.
+  The wind speed is ${apiResponse.wind.speed} miles/hour.`;
 }
 
 function handleFormSubmission(event) {
