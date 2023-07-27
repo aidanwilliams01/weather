ae4997f3b163102a6ecd70208b572d17f7e0ced6
Aidan Williams <aidanwilliams01@gmail.com>
2023-07-27 16:22:11 -0700

    add initial forecast functionality

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ function getWeather(location) {
   request.addEventListener("loadend", function() {
     const response = JSON.parse(this.responseText);
     if (this.status === 200) {
-      printElements(response, location);
+      printWeather(response, location);
     } else {
       printError(this, response, location);
     }
@@ -28,13 +28,37 @@ function getWeather(location) {
   }
 }
 
+function getFutureWeather(location) {
+  let request = new XMLHttpRequest();
+
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printFutureWeather(response);
+    } else {
+      printError(this, response, location);
+    }
+  });
+
+  if (location.search(/\d/) === 0) {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?zip=${location}&cnt=8&appid=${process.env.API_KEY}&units=imperial`;
+    request.open("GET", url, true);
+    request.send();
+  }
+  else {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&cnt=8&appid=${process.env.API_KEY}&units=imperial`;
+    request.open("GET", url, true);
+    request.send();
+  }
+}
+
 // UI Logic
 
 function printError(request, apiResponse, location) {
   document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${location}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
 }
 
-function printElements(apiResponse, location) {
+function printWeather(apiResponse, location) {
   document.querySelector('#showResponse').innerText = `The weather in ${location} is '${apiResponse.weather[0].description}'.
   The humidity is ${apiResponse.main.humidity}%.
   The temperature in Fahrenheit is ${apiResponse.main.temp} degrees.
@@ -42,11 +66,19 @@ function printElements(apiResponse, location) {
   The wind speed is ${apiResponse.wind.speed} miles/hour.`;
 }
 
+function printFutureWeather(apiResponse) {
+  let dateTime =  new Date((apiResponse.list[1].dt) * 1000);
+  // dateTime = `${dateTime.getUTCHours()}${dateTime.getUTCMinutes()}`;
+  document.querySelector('#showFutureWeather').innerText = `Twelve-hour forecast:
+  ${dateTime}`;
+}
+
 function handleFormSubmission(event) {
   event.preventDefault();
   const location = document.querySelector('#location').value;
   document.querySelector('#location').value = null;
   getWeather(location);
+  getFutureWeather(location);
 }
 
 window.addEventListener("load", function() {
